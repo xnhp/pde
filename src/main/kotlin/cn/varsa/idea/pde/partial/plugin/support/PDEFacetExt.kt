d7fc0c41a5232bbeb09107b9c719e03e05858b60
Benjamin Moser <benjamin.moser@knime.com>
2025-10-02 16:21:31 +0200

    refactor: adjust import to use core BundleManifest ecosystem (DevModule only)

--- src/main/kotlin/cn/varsa/idea/pde/partial/plugin/support/PDEFacetExt.kt
+++ src/main/kotlin/cn/varsa/idea/pde/partial/plugin/support/PDEFacetExt.kt
@@ -1,6 +1,6 @@
 package cn.varsa.idea.pde.partial.plugin.support
 
-import cn.varsa.idea.pde.partial.common.domain.*
+import cn.varsa.idea.pde.partial.common.domain.DevModule
 import cn.varsa.idea.pde.partial.common.support.*
 import cn.varsa.idea.pde.partial.plugin.cache.*
 import cn.varsa.idea.pde.partial.plugin.facet.*
